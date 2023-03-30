ac777bc87acd5c731109050a8522c0408f99ebe8
Nelnico <nelnico008@gmail.com>
2023-03-30 11:37:41 -0300

    change server connection force to true

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app");
 const { conn } = require("./src/db");
 
-conn.sync( {force: false}).then(() => {
+conn.sync( {force: true}).then(() => {
   server.listen(process.env.PORT, () =>{
     console.log(`listening at ${process.env.PORT}`)
   })
